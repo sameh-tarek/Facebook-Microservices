28fa4093bfa4360f004a19d86f506e2a84f1573b
sameh-tarek <tarksame7@gmail.com>
2024-11-04 21:24:36 +0200

    user-service -> start user-service config and entities

---
+++ User-Service/src/main/java/org/microservice/userservice/entity/User.java
@@ -0,0 +1,36 @@
+package org.microservice.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "profile_picture_url")
+    private String profilePictureUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
