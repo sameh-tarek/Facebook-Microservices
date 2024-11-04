70a0723fa540d91dd8c3b469c22116b958f0a61c
sameh-tarek <tarksame7@gmail.com>
2024-11-04 22:09:13 +0200

    comment-servie -> start comment-service config and entities

---
+++ Comment-service/src/main/java/org/microservice/commentservice/entity/Comment.java
@@ -0,0 +1,33 @@
+package org.microservice.commentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
